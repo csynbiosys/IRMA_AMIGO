b992c8b59f7182401f8c188d3dbdf3ce62f7e3b3
deeptiVipin <s1565325@sms.ed.ac.uk>
2016-08-29 13:05:48 +0100

    Update irma_oed.m

--- irma_oed.m
+++ irma_oed.m
@@ -1,11 +1,4 @@
 irma_model  %Loads model related data
-%======================
-%PATHS RELATED DATA
-%======================
-
-inputs.pathd.results_folder='irma_res_Thesis';         % Folder to keep results (in Results) for a given problem          
-inputs.pathd.short_name='irma';                      % To identify figures and reports for a given problem   
-
 
 %==================================
 % EXPERIMENTAL SCHEME RELATED DATA
